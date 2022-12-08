ab46730bf7b08aa3011704babb677d6448825810
Sesirajnaidu <sesirajn@gmail.com>
2022-12-08 16:30:27 +0530

    redux package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import SendingData from"./SendingData"
-
+//import SendingData from"./SendingData"
+//import counter from "./counter"
+import {useSelector,useDispatch} from "react-redux"
+import{increment,decrement} from "./actions"
 function App(){
+  const count=useSelector(state=>state.counter)
+  const dispatch=useDispatch()
   return(
-    <SendingData/>
-
+    <div style={{textAlign:"center"}}>
+      <h1>{count}</h1>
+      <button onClick={()=>dispatch(decrement())}>-</button>
+      <button onClick={()=>dispatch(increment())}>+</button>
+    </div>
   )
 }
 export default App
